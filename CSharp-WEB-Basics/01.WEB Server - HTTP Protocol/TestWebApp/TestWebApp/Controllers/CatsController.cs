36bd0e17e4c83252d3d61d8a875582f853cb4575
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-05-22 16:52:58 +0200

    TestWebApp
    Upload

---
+++ CSharp-WEB-Basics/01.WEB Server - HTTP Protocol/TestWebApp/TestWebApp/Controllers/CatsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    public class CatsController : Controller
+    {
+        public IActionResult List()
+        {
+            return Redirect("/cats/search");
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
