a4036fc21af80af9b5007a1a838ee7c232464b25
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-07-11 17:39:54 +0200

    Removing unnecessary services and change the Bike.All column names

--- ASP.NETCore/Demo/TestApplication/TestApplication/Controllers/BikesController.cs
+++ ASP.NETCore/Demo/TestApplication/TestApplication/Controllers/BikesController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TestApplication.Data;
 using TestApplication.Data.Models;
-using TestApplication.Models;
 using TestApplication.Models.Bikes;
-using TestApplication.Services.Bikes;
 
 namespace TestApplication.Controllers
 {
