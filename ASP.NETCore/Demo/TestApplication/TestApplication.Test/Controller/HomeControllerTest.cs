33960d8a92410f220fdda720e79a2557f1e9bd69
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-01-25 20:40:46 +0100

    UnitTests

---
+++ ASP.NETCore/Demo/TestApplication/TestApplication.Test/Controller/HomeControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TestApplication.Controllers;
+using Xunit;
+
+namespace TestApplication.Test.Controller
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void ErrorShouldReturnView()
+        {
+            //Arange
+            var mock = new Mock<ILogger<HomeController>>();
+            ILogger<HomeController> logger = mock.Object;
+
+            var homeController = new HomeController(logger, null);
+
+            //Act
+            var result = homeController.Error();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
